92fe0fd0440ea31eabe95eb41c05eec61fee824a
Brian Picciano <BJPicciano@gmail.com>
2016-03-08 11:51:51 -0500

    added UserAuthentication

---
+++ src/Portfolio/UserAuthentication.java
@@ -0,0 +1,10 @@
+package Portfolio;
+
+/**
+ * Created by Brian on 3/8/2016.
+ */
+public class UserAuthentication {
+    public static void main(String[] args) {
+        System.out.println("nice java");
+    }
+}
