3762c3fcc3ba5455dd8f3ad54925be3efef994b5
Ryan Bell <42rbell@gmail.com>
2016-03-09 12:12:13 -0500

    functionality for hashing and checking passwords

--- src/Portfolio/UserAuthentication.java
+++ src/Portfolio/UserAuthentication.java
@@ -2,50 +2,72 @@ package Portfolio;
 import View.View;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 /**
  * Handles user creation, logging in and creating portfolio. Contains
  * the main.
  */
 public class UserAuthentication {
-    public static void main(String[] args) {
-        System.out.println("program Start");
 
-        //create a instance to be able to call methods
-        UserAuthentication userAuthentication = new UserAuthentication();
-        //create the login page
-        userAuthentication.createUI();
+    public boolean checkPassword(String id, String pass){
+        String hashedPass =  hash(pass);
+        ArrayList<String> portfolio = getPOFromId(id);
+        //check that the portfolio has the same hash
+        return (portfolio.get(0).equals(hashedPass));
     }
 
-
-    File getPOFromId(){
-        //stub
-        return new File("empty.txt");
+    public ArrayList<String> getPOFromId(String id){
+        ArrayList<String> portfolio = new ArrayList<>();
+        try (Stream<String> lines = Files.lines(Paths.get("./portfolios/" + id + ".txt"), Charset.defaultCharset())) {
+            lines.forEach(portfolio::add);
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        } catch (SecurityException e){
+            System.out.println(e.getMessage());
+        }
+        return portfolio;
     }
 
-    boolean checkPassword(String pass){
-        //stub
-        return true;
-    }
-
-    void deleteId(String id){
-
-    }
-
-    void createId(String id, String pass){
+    public void deleteId(String id){
 
     }
 
-    int hash(String pass){
-        //stub
-        return 1;
+    public void createId(String id, String pass){
+
     }
 
-    void createUI(){
-        View.launch(View.class); //Calls the static version of launch
+    public String hash(String pass){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.update(pass.getBytes("UTF-8"));
+            byte[] hash = digest.digest();
+
+            char[] HEX_CHARS = "0123456789ABCDEF".toCharArray();
+
+            StringBuilder sb = new StringBuilder(hash.length * 2);
+            for (byte b : hash) {
+                sb.append(HEX_CHARS[(b & 0xF0) >> 4]);
+                sb.append(HEX_CHARS[b & 0x0F]);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e){
+            System.out.println(e.getMessage());
+        } catch (UnsupportedEncodingException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
-    void createPortfolio(){
+    public void createPortfolio(){
 
     }
 
