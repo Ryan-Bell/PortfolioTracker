f5f62147cacae924c852c585b5c726075cf58cfa
Ryan Bell <42rbell@gmail.com>
2016-03-09 12:11:40 -0500

    change entry point to view and link up the login button to check pass

--- src/View/View.java
+++ src/View/View.java
@@ -1,5 +1,6 @@
 package View;
 
+import Portfolio.UserAuthentication;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -16,9 +17,19 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class View extends Application {
+    public static void main(String[] args) {
+        System.out.println("program Start");
+
+        //create the login page
+        View.launch(View.class);
+
+    }
 
     @Override
     public void start(Stage primaryStage) {
+        //create a instance to be able to call methods
+        UserAuthentication userAuthentication = new UserAuthentication();
+
         primaryStage.setTitle("Login");
 
         GridPane grid = new GridPane();
@@ -48,12 +59,28 @@ public class View extends Application {
         hbLoginBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbLoginBtn.getChildren().add(loginBtn);
         grid.add(hbLoginBtn, 1, 4);
+        loginBtn.setOnAction((event -> {
+            //call back to userAuth to check that id and pass are correct
+            //use return type for errors or new state
+            if (userAuthentication.checkPassword(userTextField.getText(), pwBox.getText())){
+                //login is successful
+                System.out.println("Login is successful");
+            }  else{
+                //login is unsuccessful
+                System.out.println("Login is NOT successful");
+            }
+
+        }));
 
         Button registerBtn = new Button("Register");
         HBox hbRegisterBtn = new HBox(10);
         hbRegisterBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbRegisterBtn.getChildren().add(registerBtn);
         grid.add(hbRegisterBtn, 0, 4);
+        registerBtn.setOnAction((event -> {
+            //call back to createId to make account
+            //use return type for errors or new portfolio
+        }));
 
         Scene scene = new Scene(grid, 500, 475);
         primaryStage.setScene(scene);
