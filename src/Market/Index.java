9a20be6e24eccad630f08cf7234bdf33f87c8210
Ryan Bell <42rbell@gmail.com>
2016-03-09 20:11:27 -0500

    add constructor and addChild function to index class

--- src/Market/Index.java
+++ src/Market/Index.java
@@ -1,37 +1,51 @@
 package Market;
 
+import java.util.ArrayList;
+
 /**
  * The composite of MarketEquity.
  * extends MarketEquity to allow both Indexes
  * and Equities to be treated the same.
  */
 public class Index extends MarketEquity {
-    private MarketEquity[] children;
+    private ArrayList<MarketEquity> children;
+
+    /**
+     * Constructor
+     */
+    public Index(String n){
+        value = 0;
+        name = n;
+        children = new ArrayList<>();
+    }
 
     /**
     * Adds a child MarketEquity to children
     * @param  child  a MarketEquity object to add
     */
     public void addChildren(MarketEquity child) {
-
+        //check that child is not already in children
+        if (!children.contains(child)){
+            //add it to the array
+            children.add(child);
+            value += child.value;
+        }
     }
+
+
 
     /**
      * Gets the children field
      * @return  children    the array of MarketEquities
      */
-    public MarketEquity[] getChildren() {
+    public ArrayList<MarketEquity> getChildren() {
         return children;
     }
 
     /**
      * Gets the added value of all the children
-     * @return  value   the value of all children
+     * @return  value the value of all children
      */
     @Override
-    public float getValue() {
-        float value = 0;
-
-        return value;
-    }
+    public float getValue() { return value; }
 }
