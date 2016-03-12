48ab2da7b0dd0e454ede732b0300104d002127a7
Meghan Dwyer <mjd9008@rit.edu>
2016-03-12 13:05:48 -0500

    Added Transaction log and method stubs for add and remove Cash Account

--- src/Portfolio/Portfolio.java
+++ src/Portfolio/Portfolio.java
@@ -1,13 +1,17 @@
 package Portfolio;
 
 import Market.MarketEquity;
+import Transaction.Transaction;
+
+import java.util.ArrayList;
 
 /**
  * Contains user equityholdings and cash accounts
  */
 public class Portfolio {
-    HoldingEquity[] holdingEquities;
-    CashAccount[] cashAccounts;
+    ArrayList<HoldingEquity> holdingEquities;
+    ArrayList<CashAccount> cashAccounts;
+    ArrayList<Transaction> transactionLog;
 
     public void buyEquity(MarketEquity target, int numShares){
         //add target equity to HoldingEquity with numShares
@@ -15,5 +19,13 @@ public class Portfolio {
 
     public void sellEquity(MarketEquity target, int numShares){
         //remove target number of shares from Equity holding, **if shares goes to 0 delete object?
+    }
+
+    public void addCashAccount(CashAccount target, float initialBalance){
+        //add target cash account with initial balance
+    }
+
+    public void removeCashAccount(CashAccount target){
+        //remove target cash account
     }
 }
