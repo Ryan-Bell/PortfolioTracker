bbb297e7a1b3d595eb13bbb2c805b16e28ee837d
Ryan Bell <42rbell@gmail.com>
2016-03-12 13:37:24 -0500

    commented methods in portfolio

--- src/Portfolio/Portfolio.java
+++ src/Portfolio/Portfolio.java
@@ -2,22 +2,28 @@ package Portfolio;
 
 import Market.MarketEquity;
 import Transaction.Transaction;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Contains user equityholdings and cash accounts.
  */
-public class Portfolio {
+public class Portfolio implements Serializable{
     ArrayList<HoldingEquity> holdingEquities;
     ArrayList<CashAccount> cashAccounts;
     ArrayList<Transaction> transactionLog;
 
+    /**
+     * creates and adds a specific number of shares of the given equity to this portfolio
+     * @param target the equity that is being purchased
+     * @param numShares the number of shares that are being aquired
+     */
     public void buyEquity(MarketEquity target, int numShares){
         //add target equity to HoldingEquity with numShares
         HoldingEquity newEquity = new HoldingEquity(numShares,target.getValue());
         holdingEquities.add(newEquity);
     }
+
 
     public void sellEquity(MarketEquity target, int numShares){
         //remove target number of shares from Equity holding, **if shares goes to 0 delete object?
@@ -25,12 +31,21 @@ public class Portfolio {
         holdingEquities.remove(index);
     }
 
+    /**
+     * creates and adds a cash account object to this portfolio with the given name and initial balance
+     * @param name the name requested for the cash account
+     * @param initialBalance the starting balance of the account
+     */
     public void addCashAccount(String name, float initialBalance){
         //add target cash account with initial balance
-        CashAccount newCashAccount = new CashAccount(name,initialBalance);
+        CashAccount newCashAccount = new CashAccount(name, initialBalance);
         cashAccounts.add(newCashAccount);
     }
 
+    /**
+     * deletes a cash account from the portfolio
+     * @param target the cash account to  be deleted
+     */
     public void removeCashAccount(CashAccount target){
         //remove target cash account
         int index = cashAccounts.indexOf(target);
