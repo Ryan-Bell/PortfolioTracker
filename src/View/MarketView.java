1ad4f0b1b97126a2c53086f88a7896bdc435c8dc
Meghan Dwyer <mjd9008@rit.edu>
2016-03-12 21:59:32 -0500

    Added search type choice boxes, removed redundant code, commented class, and class methods

--- src/View/MarketView.java
+++ src/View/MarketView.java
@@ -6,6 +6,7 @@ import Market.MatchType;
 import Market.QueryType;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -15,24 +16,33 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
 
 /**
- * Created by Ryan on 3/12/2016.
+ * View for Searching and Buying MarketEquities
  */
 public class MarketView extends View {
 
     ArrayList<MarketEquity> searchResults;
 
     Text scenetitle;
-    Label searchLabel;
-    TextField searchField;
+    Label tickerSearch;
+    Label nameSearch;
+    Label indexSectorSearch;
+    TextField tickerField;
+    TextField nameField;
+    TextField indexSectorField;
     Label resultsLabel;
-    TextField resultsField;
     VBox resultList;
     ScrollPane resultView;
 
+    /**
+     * displays search fields and results
+     * @param context the current state of the application
+     */
+    //TODO only pass setResults a list that meets all search conditions
     @Override
     public void display(Context context) {
         super.display(context);
@@ -40,35 +50,70 @@ public class MarketView extends View {
 
         primaryStage.setTitle("Market");
 
-        scenetitle = new Text("Market");
+        scenetitle = new Text("Market Search");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
+        grid.add(scenetitle, 1, 0, 3, 1);
 
-        searchLabel = new Label("Search");
-        grid.add(searchLabel, 0, 1);
+        tickerSearch = new Label("Ticker");
+        grid.add(tickerSearch, 0, 1);
 
-        searchField = new TextField();
-        grid.add(searchField, 1, 1);
+        ChoiceBox tickerCB =  new ChoiceBox();
+        tickerCB.setValue("Select Query Type");
+        tickerCB.setItems(FXCollections.observableArrayList(
+                "Select Query Type",
+                new Separator(), MatchType.EXACT,MatchType.BEGIN,MatchType.CONTAINED));
+        grid.add(tickerCB, 1, 1);
 
-        resultsLabel = new Label("Results");
-        grid.add(resultsLabel, 0, 2);
+        tickerField = new TextField();
+        grid.add(tickerField, 2, 1);
 
-        resultsField = new TextField();
-        grid.add(resultsField, 1, 2);
+        nameSearch = new Label("Name");
+        grid.add(nameSearch, 0, 2);
+
+
+        ChoiceBox nameCB =  new ChoiceBox();
+        nameCB.setValue("Select Query Type");
+        nameCB.setItems(FXCollections.observableArrayList(
+                "Select Query Type",
+                new Separator(), MatchType.EXACT,MatchType.BEGIN,MatchType.CONTAINED));
+        grid.add(nameCB, 1, 2);
+
+
+        nameField = new TextField();
+        grid.add(nameField, 2, 2);
+
+        indexSectorSearch = new Label("Index or Sector");
+        grid.add(indexSectorSearch, 0, 3);
+
+
+        ChoiceBox indexSectorCB =  new ChoiceBox();
+        indexSectorCB.setValue("Select Query Type");
+        indexSectorCB.setItems(FXCollections.observableArrayList(
+                "Select Query Type",
+                new Separator(), MatchType.EXACT,MatchType.BEGIN,MatchType.CONTAINED));
+        grid.add(indexSectorCB, 1, 3);
+
+
+        indexSectorField = new TextField();
+        grid.add(indexSectorField, 2, 3);
+
+
+
+        resultsLabel = new Label("Results:");
+        grid.add(resultsLabel, 0, 4);
 
         resultView = new ScrollPane();
-        grid.add(resultView,1,3);
+        grid.add(resultView,0,5,3,1);
+
 
         resultList = new VBox();
 
 
-
-
-
-        searchField.textProperty().addListener(new ChangeListener<String>() {
+        //TODO Implement listener for each of the fields
+        tickerField.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(final ObservableValue<? extends String> observable, final String oldValue, final String newValue) {
-                setResults(context.getMarket().search(QueryType.TICKER , newValue, MatchType.CONTAINED));
+                setResults(context.getMarket().search(QueryType.TICKER , newValue, (MatchType)tickerCB.getValue()));
             }
         });
 
@@ -76,23 +121,22 @@ public class MarketView extends View {
         primaryStage.setScene(newScene);
     }
 
+    /**
+     * Displays a list of MarketEquity objects that meet current search conditions
+     * @param results list of MarketEquity objects that meet the current search conditions
+     */
     private void setResults(ArrayList<MarketEquity> results){
         searchResults = results;
-        resultsField.setText("");
-        String text = "";
         resultList = new VBox();
-        System.out.println("Did the children die?");
         for (MarketEquity e : results) {
-            text += e.getName();
             Label equity = new Label(e.getName());
-            Button buy = new Button("Buy");
+            Button buy = new Button("Buy"); //TODO Link button to buy transaction
             BorderPane equityDisplay = new BorderPane();
             equityDisplay.setLeft(equity);
             equityDisplay.setRight(buy);
 
             resultList.getChildren().add(equityDisplay);
         }
-        resultsField.setText(text);
         resultView.setContent(resultList);
     }
 }
