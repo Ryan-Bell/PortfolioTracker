915d9144bcc41d2a8167d7699cafea99a0e8a604
Ryan Bell <rjb1928@rit.edu>
2016-03-30 10:37:07 -0400

    inject login and register fields into controller

--- R2/src/sample/Controller.java
+++ R2/src/sample/Controller.java
@@ -1,4 +1,55 @@
 package sample;
 
-public class Controller {
+//region imports
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+//endregion
+
+public class Controller implements Initializable{
+    //region FXMLFields
+    //declare fxml elements for the login pane
+    @FXML
+    private TextField loginUsernameField;
+
+    @FXML
+    private PasswordField loginPasswordField;
+
+    @FXML
+    private Button loginButton;
+
+    //declare fxml elements for the registration pane
+    @FXML
+    private TextField registerUsernameField;
+
+    @FXML
+    private PasswordField registerPasswordField;
+
+    @FXML
+    private PasswordField registerConfirmField;
+
+    @FXML
+    private Button registerButton;
+    //endregion
+
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        //assert that all elements of the login pane have been properly initialized
+        assert loginUsernameField != null : "fx:id=\"loginUsernameField\" was not injected: check the associated fxml file";
+        assert loginPasswordField != null : "fx:id=\"loginPasswordField\" was not injected: check the associated fxml file";
+        assert loginButton != null : "fx:id=\"loginButton\" was not injected: check the associated fxml file";
+
+        //assert that all elements of the registration pane have been properly initialized
+        assert registerUsernameField != null : "fx:id=\"registerUsernameField\" was not injected: check the associated fxml file";
+        assert registerPasswordField != null : "fx:id=\"registerPasswordField\" was not injected: check the associated fxml file";
+        assert registerConfirmField != null : "fx:id=\"registerConfirmField\" was not injected: check the associated fxml file";
+        assert registerButton != null : "fx:id=\"registerButton\" was not injected: check the associated fxml file";
+
+        // all @FXML variables will have been injected
+        System.out.println("The FXML has been initialized");
+    }
 }
