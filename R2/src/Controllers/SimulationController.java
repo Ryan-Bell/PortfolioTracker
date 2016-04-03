563c55af1fceb2d7fd10e0a9b204bb75d4b66687
Meghan Dwyer <mjd9008@rit.edu>
2016-04-03 11:27:28 -0400

    Market, Simulation, Transaction scenes created and skeleton controller generated

--- R2/src/Controllers/SimulationController.java
+++ R2/src/Controllers/SimulationController.java
@@ -1,7 +1,59 @@
+/**
+ * Sample Skeleton for 'simulation.fxml' Controller Class
+ */
+
 package Controllers;
 
-/**
- * Created by Meg on 4/3/2016.
- */
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
 public class SimulationController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="numStepsField"
+    private TextField numStepsField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="stepType"
+    private MenuButton stepType; // Value injected by FXMLLoader
+
+    @FXML // fx:id="runSimButton"
+    private Button runSimButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="simulationType"
+    private MenuButton simulationType; // Value injected by FXMLLoader
+
+    @FXML // fx:id="percentChangeField"
+    private TextField percentChangeField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="simResultsList"
+    private ListView<?> simResultsList; // Value injected by FXMLLoader
+
+    @FXML
+    void handleRunSim(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert numStepsField != null : "fx:id=\"numStepsField\" was not injected: check your FXML file 'simulation.fxml'.";
+        assert stepType != null : "fx:id=\"stepType\" was not injected: check your FXML file 'simulation.fxml'.";
+        assert runSimButton != null : "fx:id=\"runSimButton\" was not injected: check your FXML file 'simulation.fxml'.";
+        assert simulationType != null : "fx:id=\"simulationType\" was not injected: check your FXML file 'simulation.fxml'.";
+        assert percentChangeField != null : "fx:id=\"percentChangeField\" was not injected: check your FXML file 'simulation.fxml'.";
+        assert simResultsList != null : "fx:id=\"simResultsList\" was not injected: check your FXML file 'simulation.fxml'.";
+
+    }
 }
+
--- R2/src/Controllers/TransactionLogController.java
+++ R2/src/Controllers/TransactionLogController.java
@@ -1,7 +1,50 @@
+/**
+ * Sample Skeleton for 'transactionlog.fxml' Controller Class
+ */
+
 package Controllers;
 
-/**
- * Created by Meg on 4/3/2016.
- */
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+
 public class TransactionLogController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="endDatePicker"
+    private DatePicker endDatePicker; // Value injected by FXMLLoader
+
+    @FXML // fx:id="startDatePicker"
+    private DatePicker startDatePicker; // Value injected by FXMLLoader
+
+    @FXML // fx:id="transactionList"
+    private ListView<?> transactionList; // Value injected by FXMLLoader
+
+    @FXML // fx:id="filterButton"
+    private Button filterButton; // Value injected by FXMLLoader
+
+    @FXML
+    void handleFilter(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert endDatePicker != null : "fx:id=\"endDatePicker\" was not injected: check your FXML file 'transactionlog.fxml'.";
+        assert startDatePicker != null : "fx:id=\"startDatePicker\" was not injected: check your FXML file 'transactionlog.fxml'.";
+        assert transactionList != null : "fx:id=\"transactionList\" was not injected: check your FXML file 'transactionlog.fxml'.";
+        assert filterButton != null : "fx:id=\"filterButton\" was not injected: check your FXML file 'transactionlog.fxml'.";
+
+    }
 }
+
