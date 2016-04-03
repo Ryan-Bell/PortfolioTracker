a04313bbd4a5a9f259f15a4433f142435bdcd63c
Ryan Bell <rjb1928@rit.edu>
2016-04-03 11:45:20 -0400

    extend container controller

--- R2/src/Controllers/ContainerController.java
+++ R2/src/Controllers/ContainerController.java
@@ -9,9 +9,10 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class ContainerController {
+public class ContainerController extends ViewController implements Initializable{
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -43,8 +44,8 @@ public class ContainerController {
 
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
         assert refreshButton != null : "fx:id=\"refreshButton\" was not injected: check your FXML file 'container.fxml'.";
         assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'container.fxml'.";
         assert saveButton != null : "fx:id=\"saveButton\" was not injected: check your FXML file 'container.fxml'.";
