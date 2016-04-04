a941878b1b9ba1349b6251790b57edb3e0e69de7
Ryan Bell <rjb1928@rit.edu>
2016-04-04 19:26:47 -0400

    Integrated Portfolio Momento and Simulator

---
+++ R2/src/Models/MarketSimulation/PortfolioMemento.java
@@ -0,0 +1,26 @@
+package Models.MarketSimulation;
+
+
+/**
+ * A memento pattern to save the state of a
+ * simulation in order to return to it at any time.
+ */
+public class PortfolioMemento {
+    private Simulation simulation;
+
+    /**
+     * Constructor
+     * @param   simulation    the simulation to restore to
+     */
+    public PortfolioMemento(Simulation simulation) {
+        this.simulation = simulation;
+    }
+
+    /**
+     * Returns the value of the simulated market after the simulation was ran.
+     * @return  value   the value after the simulation is ran
+     */
+    public Simulation getSimulation() {
+        return simulation;
+    }
+}
