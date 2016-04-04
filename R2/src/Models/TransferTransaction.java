ab84bb33a65983304c414fc5ebfb21dc3332e53c
Ryan Bell <rjb1928@rit.edu>
2016-04-04 19:16:00 -0400

    Integrate Transfer Transaction

---
+++ R2/src/Models/TransferTransaction.java
@@ -0,0 +1,41 @@
+package Models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Command allowing users to withdraw money from a CashAccount and add it to
+ * a different CashAccount.
+ */
+public class TransferTransaction implements Transaction, Serializable {
+
+
+    private CashAccount withdrawTarget;
+    private CashAccount depositTarget;
+    private float amount;
+    private LocalDateTime date;
+    private boolean failed;
+
+    public TransferTransaction(CashAccount withdrawTarget, CashAccount depositTarget, float amount) {
+        this.withdrawTarget = withdrawTarget;
+        this.depositTarget = depositTarget;
+        this.amount = amount;
+        this.date = LocalDateTime.now();
+        this.failed = false;
+    }
+
+    @Override
+    public void execute() {
+        if (withdrawTarget.sufficientFunds(amount)){
+            withdrawTarget.withdraw(amount);
+            depositTarget.deposit(amount);
+        }
+        else failed = true;
+    }
+
+    @Override
+    public String toString() {
+        if(failed) return "Could not withdraw "+amount+" out of "+withdrawTarget.getName()+" and deposit in "+depositTarget.getName()+" on "+date;
+        return "Withdrew "+amount+" out of "+withdrawTarget.getName()+" and deposited in "+depositTarget.getName()+" on "+date;
+    }
+}
