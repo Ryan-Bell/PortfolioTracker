b9c37a6a156f3acfe1d37ca9420d4827576297f3
Ryan Bell <rjb1928@rit.edu>
2016-04-05 11:42:54 -0400

    UndoRedo extends transaction

--- R2/src/Models/UndoRedo/UndoRedo.java
+++ R2/src/Models/UndoRedo/UndoRedo.java
@@ -1,14 +1,11 @@
 package Models.UndoRedo;
 
+import Models.Transaction.Transaction;
+
 /**
  * The interface to make use of the Undo Redo functions.
  */
-public interface UndoRedo {
-
-    /**
-     * run a command
-     */
-    void execute();
+public interface UndoRedo extends Transaction {
 
     /**
      * run the opposite of the command
