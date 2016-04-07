e7eb1fdb9a25908ba2ad7f3751ff235fce68bf15
Ryan Bell <rjb1928@rit.edu>
2016-04-07 18:34:11 -0400

    new remove cash account transaction

---
+++ R2/src/Models/Transaction/RemoveCashAccTransaction.java
@@ -0,0 +1,41 @@
+package Models.Transaction;
+
+import Models.Portfolio.CashAccount;
+import Models.Portfolio.Portfolio;
+import Models.UndoRedo.UndoRedo;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class RemoveCashAccTransaction implements UndoRedo, Serializable {
+    private CashAccount cashAccount;
+    private Portfolio portfolio;
+    private LocalDateTime date;
+    private boolean failed;
+
+
+    public RemoveCashAccTransaction(CashAccount cashAccount, Portfolio portfolio) {
+        this.cashAccount = cashAccount;
+        this.portfolio  = portfolio;
+        date = LocalDateTime.now();
+        this.failed = false;
+    }
+
+    @Override
+    public void execute() {
+        if(portfolio.getCashAccNameExists(cashAccount.getName()) != -1) portfolio.removeCashAccount(cashAccount);
+        else failed = true;
+    }
+
+    @Override
+    public String toString() {
+        if(failed) return "Could not remove Cash Account '" + cashAccount.getName() + "' with balance of " + cashAccount.getBalance() + " on " + date;
+        return "Removed Cash Account '" + cashAccount.getName() + "' with balance of " + cashAccount.getBalance() + " on " + date;
+    }
+
+    @Override
+    public void unExecute() {
+        portfolio.addCashAccount(cashAccount.getName(), cashAccount.getBalance());
+    }
+
+}
