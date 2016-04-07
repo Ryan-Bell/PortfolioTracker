5fc3bf8a645d979d9880e9e5cde855abf3f7e6a4
Ryan Bell <rjb1928@rit.edu>
2016-04-07 19:39:17 -0400

    Commented Deposit and RemoveCashAcc

--- R2/src/Models/Transaction/DepositTransaction.java
+++ R2/src/Models/Transaction/DepositTransaction.java
@@ -11,28 +11,49 @@ import java.time.LocalDateTime;
  */
 public class DepositTransaction implements UndoRedo, Serializable {
 
+    //region Fields
+    //The cash account to add  funds to
     private CashAccount target;
-    private float amount;
-    private LocalDateTime date;
 
+    //the amount of money to add
+    private float amount;
+
+    //the time this command was created
+    private LocalDateTime date;
+    //endregion
+
+    /** Constructor
+     * @param target the cash account to add funds to
+     * @param amount the amount of funds to add
+     */
     public DepositTransaction(CashAccount target, float amount){
         this.target = target;
         this.amount = amount;
         this.date = LocalDateTime.now();
     }
 
+    /**
+     * Adds the funds to the cash account
+     */
     @Override
     public void execute() {
         target.deposit(amount);
     }
 
+    /** Return a string representation of this command
+     * @return the string representation
+     */
     @Override
     public String toString() {
-        return "Deposited "+amount+" into "+target.getName()+" on "+date;
+        return "Deposited " + amount + " into " + target.getName() + " on " + date;
     }
 
+    /**
+     * Reverts the action of the execute method
+     */
     @Override
     public void unExecute() {
+        //withdraw the money that was added with execute
         target.withdraw(amount);
     }
 }
