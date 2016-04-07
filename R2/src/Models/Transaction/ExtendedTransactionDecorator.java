8309fe479dea6c96d87b8bccbb4001ae4a2d1a53
Ryan Bell <rjb1928@rit.edu>
2016-04-07 19:45:05 -0400

    Commented decorator abstract classed

--- R2/src/Models/Transaction/ExtendedTransactionDecorator.java
+++ R2/src/Models/Transaction/ExtendedTransactionDecorator.java
@@ -2,21 +2,38 @@ package Models.Transaction;
 
 import Models.UndoRedo.UndoRedo;
 
+/**
+ * Abstract class that defines behaviour for decorators of UndoRedo Objects
+ */
 public abstract class ExtendedTransactionDecorator implements UndoRedo{
 
+    //region Fields
+    //the UndoRedo object that is being decorated
     protected UndoRedo transactionToBoDecorated;
-    protected boolean failed;
 
+    //the failure status of the action specific to this decorator
+    protected boolean failed;
+    //endregion
+
+    /** Constructor
+     * @param decoratorTarget the object being decorated
+     */
     public ExtendedTransactionDecorator(UndoRedo decoratorTarget){
         transactionToBoDecorated = decoratorTarget;
         failed = false;
     }
 
+    /**
+     * Base unExecute just calls unExecute of the decoratee if not overridden
+     */
     @Override
     public void unExecute() {
         transactionToBoDecorated.unExecute();
     }
 
+    /**
+     * Base execute just calls execute of the decoratee if not overridden
+     */
     @Override
     public void execute() {
         transactionToBoDecorated.execute();
--- R2/src/Models/Transaction/SimpleTransactionDecorator.java
+++ R2/src/Models/Transaction/SimpleTransactionDecorator.java
@@ -1,15 +1,29 @@
 package Models.Transaction;
 
+/**
+ * Abstract class that defines behaviour for decorators of Transaction Objects
+ */
 public abstract class SimpleTransactionDecorator implements Transaction {
 
+    //region Fields
+    //The transaction that is to be decorated
     private Transaction transactionToBeDecorated;
-    protected boolean failed;
 
+    //the failure status specific to this decorator
+    protected boolean failed;
+    //endregion
+
+    /** Constructer
+     * @param decoratorTarget the object being decorated
+     */
     public SimpleTransactionDecorator(Transaction decoratorTarget){
         transactionToBeDecorated = decoratorTarget;
         failed = false;;
     }
 
+    /**
+     * Base execute just calls execute of the decoratee if not overridden
+     */
     @Override
     public void execute() {
         transactionToBeDecorated.execute();
