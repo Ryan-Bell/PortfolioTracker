1dc4daf5457fffddf5870c4e44e578b4da9038c9
Ryan Bell <rjb1928@rit.edu>
2016-04-08 15:00:53 -0400

    commented import controller

--- R2/src/Controllers/ImportController.java
+++ R2/src/Controllers/ImportController.java
@@ -1,7 +1,3 @@
-/**
- * Sample Skeleton for 'import.fxml' Controller Class
- */
-
 package Controllers;
 
 import Models.FileIO.PortfolioIO;
@@ -23,6 +19,9 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+/**
+ * Controller for the import view
+ */
 public class ImportController extends ViewController implements Initializable {
 
     //region FXMLFields
@@ -39,7 +38,11 @@ public class ImportController extends ViewController implements Initializable {
 
     //a portfolio IO  object to read in the other portfolio
     PortfolioIO portfolioIO;
+
+    //List that will contain the conflicting cash accounts
     ArrayList<CashAccount> conflicts;
+
+    //List that will store the result outputs
     ArrayList<String> results;
 
     @Override // This method is called by the FXMLLoader when initialization is complete
@@ -69,32 +72,72 @@ public class ImportController extends ViewController implements Initializable {
         portfolioTab.setDisable(false);
     }
 
+    /**
+     * Action listener for the merge button
+     */
     @FXML
     void handleMerge(){
+        //capture the currently selected cash account from the conflict list
         CashAccount mergeAccount = importConflictsList.getSelectionModel().getSelectedItem();
+
+        //execute a new deposit transaction to move the funds from one account into the other
         UndoRedoFunctions.getInstance().execute(new DepositTransaction(main.getPortfolio().getCashAccounts().get(main.getPortfolio().getCashAccNameExists(mergeAccount.getName())), mergeAccount.getBalance()));
+
+        //display to the user that the merge took place
         results.add("Merged Cash Accounts '"+ mergeAccount.getName() + "'");
+
+        //remove the imported account from the conflict list
         conflicts.remove(mergeAccount);
+
+        //update the conflict list
         displayConflicts();
     }
 
+    /**
+     * Action listener for the replace button
+     */
     @FXML
     void handleReplace(){
-        main.getPortfolio().removeCashAccount(main.getPortfolio().getCashAccounts().get(main.getPortfolio().getCashAccNameExists(importConflictsList.getSelectionModel().getSelectedItem().getName())));
+        //capture the currently selected account in the conflict list
         CashAccount replacementAccount = importConflictsList.getSelectionModel().getSelectedItem();
+
+        //remove the currently selected cash account from the portfolio
+        main.getPortfolio().removeCashAccount(main.getPortfolio().getCashAccounts().get(main.getPortfolio().getCashAccNameExists(replacementAccount.getName())));
+
+        //execute a new add cash account action to add the account to the portfolio
         UndoRedoFunctions.getInstance().execute(new AddCashAccTransaction(replacementAccount.getName(), replacementAccount.getBalance(), main.getPortfolio()));
+
+        //display to the user that the replacement took place
         results.add("Replaced Cash Account '" + replacementAccount.getName() + "'");
+
+        //remove the account from the conflict list
         conflicts.remove(replacementAccount);
+
+        //update the conflict list
         displayConflicts();
     }
 
+    /**
+     * Action listener for the ignore button
+     */
     @FXML
     void handleIgnore() {
-        results.add("Ignored Cash Account '" + importConflictsList.getSelectionModel().getSelectedItem().getName() + "'");
-        conflicts.remove(importConflictsList.getSelectionModel().getSelectedItem());
+        //capture the currently selected account from the conflicts list
+        CashAccount ignoredAccount = importConflictsList.getSelectionModel().getSelectedItem();
+
+        //display to the user that the account was ignored
+        results.add("Ignored Cash Account '" + ignoredAccount.getName() + "'");
+
+        //remove the account from the conflict list
+        conflicts.remove(ignoredAccount);
+
+        //update the conflict list
         displayConflicts();
     }
 
+    /**
+     * Action listener for the Import button
+     */
     @FXML
     void handleImport() {
 
@@ -107,44 +150,77 @@ public class ImportController extends ViewController implements Initializable {
             return;
         }
 
+        //call methods for importing the equities, transactions, and cash accounts
         importEquities(importPortfolio);
         importTransactions(importPortfolio);
         importCashAccounts(importPortfolio);
+
+        //show the cash account conflict list
         displayConflicts();
     }
 
+    /**
+     * Updates the two lists
+     */
     private void displayConflicts(){
         importConflictsList.setItems(FXCollections.observableList(conflicts));
         importResultsList.setItems(FXCollections.observableList(results));
     }
 
+    /** Handles the importation of the equities only
+     * @param importPortfolio the portfolio being read in
+     */
     private void importEquities(Portfolio importPortfolio){
         //add all of the imported equities to the existing portfolio
         for(HoldingEquity equity : importPortfolio.getHoldingEquities()){
+            //keeps track of the position of the currently equity in the portfolio or -1 if it doesn't exist
             int currentIndex;
+
+            //holds the portfolio's  version of the current equity
             HoldingEquity currentEquity;
+
+            //check that the imported equity is already in the portfolio
             if((currentIndex = main.getPortfolio().getEquityNameExists(equity.getName())) != -1){
+                //save the portfolio's version of the equity
                 currentEquity = main.getPortfolio().getHoldingEquities().get(currentIndex);
+
+                //add the imported number of shares to the portfolio's version
                 currentEquity.setNumShares(currentEquity.getNumShares() + equity.getNumShares());
+
+                //show the user that the shares were added
                 results.add("Added " + equity.getNumShares() + " shares to " + equity.getName());
             } else {
+                //if the portfolio doesn't already have its own version, execute a new buy transaction
                 UndoRedoFunctions.getInstance().execute(new BuyTransaction(equity, equity.getNumShares(), main.getPortfolio()));
+
+                //show the user that the shares were added
                 results.add("Added " + equity.getNumShares() +  " shares to " + equity.getName());
             }
         }
     }
 
+    /** Handles importing the transactions only
+     * @param importPortfolio the portfolio being read in
+     */
     private void importTransactions(Portfolio importPortfolio){
         //append all of the imported transactions to the existing list
         main.getPortfolio().getTransactionLog().addAll(importPortfolio.getTransactionLog());
     }
 
+    /** Handles importing the cash accounts only
+     * @param importPortfolio
+     */
     private void importCashAccounts(Portfolio importPortfolio){
+        //go through all of the cash accounts in the imported portfolio
         for(CashAccount account : importPortfolio.getCashAccounts()){
+            //check if the main portfolio has an account with the same name or not
             if(main.getPortfolio().getCashAccNameExists(account.getName()) != -1){
                 conflicts.add(account);
             } else {
+                //execute a new add cash account command if there is no name conflict
                 UndoRedoFunctions.getInstance().execute(new AddCashAccTransaction(account.getName(), account.getBalance(), main.getPortfolio()));
+
+                //show the user that the account was added
                 results.add("Added Cash Account '" + account.getName() + "' with balance of " + account.getBalance());
             }
         }
