b1a640393de35b129c6f979204c73ce273dd7bb7
Meghan Dwyer <mjd9008@rit.edu>
2016-04-08 20:30:12 -0400

    Updated market.fxml and marketcontroller to use choice boxes, updated import to match style

--- R2/src/Controllers/MarketController.java
+++ R2/src/Controllers/MarketController.java
@@ -16,10 +16,7 @@ public class MarketController extends ViewController implements Initializable {
     //region FXMLFields
     @FXML private ResourceBundle resources;
     @FXML private URL location;
-    @FXML private MenuButton indexQueryType;
-    @FXML private MenuButton nameQueryType;
     @FXML private Button searchButton;
-    @FXML private ChoiceBox tickerQueryType;
     @FXML private TextField indexSearchField;
     @FXML private TextField nameSearchField;
     @FXML private ListView<?> searchResultsField;
@@ -27,18 +24,18 @@ public class MarketController extends ViewController implements Initializable {
     @FXML private TextField cashAccountField;
     @FXML private Button buyButton;
     @FXML private TextField tickerSearchField;
-
-    @FXML
-    private ChoiceBox<MatchType> tickerChoiceBox;
+    @FXML private ChoiceBox<MatchType> nameChoiceBox;
+    @FXML private ChoiceBox<MatchType> indexChoiceBox;
+    @FXML private ChoiceBox<MatchType> tickerChoiceBox;
     //endregion
 
     @Override // This method is called by the FXMLLoader when initialization is complete
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
         //region Asserts
-        assert indexQueryType != null : "fx:id=\"indexQueryType\" was not injected: check your FXML file 'market.fxml'.";
-        assert nameQueryType != null : "fx:id=\"nameQueryType\" was not injected: check your FXML file 'market.fxml'.";
+        assert nameChoiceBox != null : "fx:id=\"indexQueryType\" was not injected: check your FXML file 'market.fxml'.";
+        assert indexChoiceBox != null : "fx:id=\"nameQueryType\" was not injected: check your FXML file 'market.fxml'.";
         assert searchButton != null : "fx:id=\"searchButton\" was not injected: check your FXML file 'market.fxml'.";
-        assert tickerQueryType != null : "fx:id=\"tickerQueryType\" was not injected: check your FXML file 'market.fxml'.";
+        assert tickerChoiceBox != null : "fx:id=\"tickerQueryType\" was not injected: check your FXML file 'market.fxml'.";
         assert indexSearchField != null : "fx:id=\"indexSearchField\" was not injected: check your FXML file 'market.fxml'.";
         assert nameSearchField != null : "fx:id=\"nameSearchField\" was not injected: check your FXML file 'market.fxml'.";
         assert searchResultsField != null : "fx:id=\"searchResultsField\" was not injected: check your FXML file 'market.fxml'.";
@@ -50,6 +47,10 @@ public class MarketController extends ViewController implements Initializable {
 
         tickerChoiceBox.setItems(FXCollections.observableArrayList(MatchType.EXACT, MatchType.CONTAINED, MatchType.BEGIN));
         tickerChoiceBox.setValue(MatchType.CONTAINED);
+        indexChoiceBox.setItems(FXCollections.observableArrayList(MatchType.EXACT, MatchType.CONTAINED, MatchType.BEGIN));
+        indexChoiceBox.setValue(MatchType.CONTAINED);
+        nameChoiceBox.setItems(FXCollections.observableArrayList(MatchType.EXACT, MatchType.CONTAINED, MatchType.BEGIN));
+        nameChoiceBox.setValue(MatchType.CONTAINED);
     }
 
     @FXML
